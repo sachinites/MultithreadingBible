2a8f3fba1dddd696393c5a2db3216a4f12c0f95f
Abhishek Sagar <sachinites@gmail.com>
2020-12-23 12:22:22 -0800

    clean up handler Demo

--- ThreadBasics/ThreadCancellation/compile.sh
+++ ThreadBasics/ThreadCancellation/compile.sh
@@ -4,3 +4,5 @@ gcc -g -c master_slave1.c -o master_slave1.o
 gcc -g -c master_slave1_async_cancellation.c -o master_slave1_async_cancellation.o
 gcc -g master_slave1.o -o master_slave1.exe -lpthread
 gcc -g master_slave1_async_cancellation.o -o master_slave1_async_cancellation.exe -lpthread
+gcc -g -c master_slave1_async_cancellation_cleanup_handlers.c -o master_slave1_async_cancellation_cleanup_handlers.o
+gcc -g master_slave1_async_cancellation_cleanup_handlers.o -o master_slave1_async_cancellation_cleanup_handlers.exe -lpthread
