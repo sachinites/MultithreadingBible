2d69d52ac3ba2c8e4374ac4ee3af29f32a13fa7e
Abhishek Sagar <sachinites@gmail.com>
2021-02-10 12:15:04 -0800

    Update compile.sh file

--- ProducerConsumer/compile.sh
+++ ProducerConsumer/compile.sh
@@ -1,3 +1,5 @@
 gcc -g -c Queue.c -o Queue.o
 gcc -g -c Assignment_prod_cons_on_Q.c -o Assignment_prod_cons_on_Q.o
+gcc -g -c Assignment_prod_cons_on_Q_Solution.c -o Assignment_prod_cons_on_Q_Solution.o
 gcc -g Assignment_prod_cons_on_Q.o Queue.o -o exe -lpthread
+gcc -g Assignment_prod_cons_on_Q_Solution.o Queue.o -o solution.exe -lpthread
