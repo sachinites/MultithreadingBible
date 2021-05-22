217b772fd640a3c9b90565c57c1a395d8408c710
sachinites <sachinites@gmail.com>
2021-05-22 05:36:28 -0700

    Wait Queues Stage set

---
+++ ThreadSyncAdv/threadlib/CourseDev/section_thread_barrier/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "threadbarrier.h"
+
+static th_barrier_t th_barrier;
+
+void *
+thread_fn_callback (void *arg) {
+    
+    thread_barrier_wait(&th_barrier);
+    
+    printf("Ist barricade cleared by thread %s\n", (char *)arg);
+
+    thread_barrier_wait(&th_barrier);
+    
+    printf("2nd barricade cleared by thread %s\n", (char *)arg);
+
+    thread_barrier_wait(&th_barrier);
+    
+    printf("3rd barricade cleared by thread %s\n", (char *)arg);
+    
+    pthread_exit(0);
+    return NULL;
+}
+
+static pthread_t pthreads[3];
+int main()
+{
+    thread_barrier_init(&th_barrier, 3);
+    
+    static const char *th1 = "th1";
+    pthread_create(&pthreads[0], NULL, thread_fn_callback, (void *)th1);
+    
+    static const char *th2 = "th2";
+    pthread_create(&pthreads[1], NULL, thread_fn_callback, (void *)th2);
+    
+    static const char *th3 = "th3";
+    pthread_create(&pthreads[2], NULL, thread_fn_callback, (void *)th3);
+    
+    pthread_join(pthreads[0], NULL);
+    pthread_join(pthreads[1], NULL);
+    pthread_join(pthreads[2], NULL);
+
+    thread_barrier_print(&th_barrier);
+    return 0;
+}
